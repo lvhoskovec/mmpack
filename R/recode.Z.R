a1d9ad98ecdf74e1164d1535c5560504692f09f9
Lauren Hoskovec <lvheck@rams.colostate.edu>
2020-04-28 15:14:24 -0600

    .Rbuildignore problems

--- R/recode.Z.R
+++ R/recode.Z.R
@@ -9,7 +9,6 @@ recode.Z <- function(Z.best){
   
   # retains the order of Z
   # appropriate to use on theta.star in bestcluster
-  
   levels <- sort(unique(Z.best))
   for (i in 1:length(levels)){
     Z.best[which(Z.best == levels[i])] <- i
